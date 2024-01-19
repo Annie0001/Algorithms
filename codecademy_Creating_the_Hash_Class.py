c4423fe02953e80e68fda963900eeb35522817bd
Annie <aniohanian@gmail.com>
2024-01-18 23:52:42 -0800

    Creating the Hashing Function

--- codecademy_Creating_the_Hash_Class.py
+++ codecademy_Creating_the_Hash_Class.py
@@ -11,3 +11,17 @@ class HashMap:
     def __init__(self,array_size):
         self.array_size = array_size
         self.array = [None for i in range(self.array_size)]
+
+###### Creating the Hashing Function ########
+#   4-  Create a method for HashMap called .hash(). This method should take two arguments: self and key.
+#   5-  Turn the key into a list of bytes by calling key.encode(). Save this into a variable called key_bytes.
+#   .encode() is a string method that converts a string into its corresponding bytes, a list-like object with the numerical representation of each character in the string.
+#   6-  Turn the bytes object into a hash code by calling sum() on key_bytes. Save the result from that into a variable called hash_code.
+#   7-  Return hash_code.
+
+    def hash(self,key):
+        # pass
+        key_bytes = key.encode()
+        # hash_code = key_bytes.sum()
+        hash_code = sum(key_bytes)
+        return hash_code
